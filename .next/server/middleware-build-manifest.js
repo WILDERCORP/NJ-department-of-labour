fb448099587b8eddac38ef78c9180d074ecefbd3
WILDER <beeowensby850@gmail.com>
2025-08-01 17:12:42 +0100

    Refactor code structure for improved readability and maintainability

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -34,11 +34,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/onboarding.js"
-    ],
-    "/verify": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/verify.js"
     ]
   },
   "ampFirstPages": []
